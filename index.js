08969838a1cfdc16fe91001c5c2bc3ccd4f9b35b
sundaramhpe <sunram_c@hotmail.com>
2020-08-13 16:52:46 +0530

    db establized

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+//set up express 
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT =process.env.PORT || 5000; 
+app.listen(PORT,()=> console.log(`The srver has started on port : ${PORT}`));
+
+// set up mongoose 
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING,
+{useNewUrlParser: true,useUnifiedTopology: true,useUnifiedTopology: true },
+(err)=>{
+   if(err) throw err;//{console.log('Db not connected') }
+   console.log('DB connected ') 
+});
